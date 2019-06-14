b0c58c7cf36b93cd300b2b445a823453b55e43e0
florian-huber <36473328+florian-huber@users.noreply.github.com>
2019-06-14 17:45:00 +0200

    Add function to detect bad episodes and channels from EEG signals

--- helper_functions.py
+++ helper_functions.py
@@ -43,3 +43,47 @@ def select_bad_channels(data_raw, time = 100, threshold = 5, include_for_mean = 
 
 
 
+def select_bad_episodes(signals, threshold = 5, max_bad_episodes = 10):
+    """
+    Function to find suspect episodes and channels --> still might need manual inspection!
+    
+    Args:
+    --------
+    signals: epochs.get_data() array (numpy array of episodes)
+        
+    threshold: float/int
+        Relative threshold. Anything channel with variance > threshold*mean OR < threshold*mean
+        will be considered suspect. Default = 5.   
+    max_bad_episodes: int
+        Maximum number of bad episodes. If number is higher for one channel, call it a 'bad' channel
+    """
+    bad_episodes = set()
+    bad_channels = []
+    
+    from collections import Counter
+    
+    # Find outliers in episode STD and max-min difference:
+    signals_std = np.std(signals, axis=2)
+    signals_minmax = np.amax(signals, axis=2) - np.amin(signals, axis=2)
+    
+    outliers = np.where((signals_std > threshold*np.mean(signals_std)) | (signals_minmax > threshold*np.mean(signals_minmax)))
+    
+    if len(outliers[0]) > 0:
+        print("Found", len(set(outliers[0])), "bad episodes in a total of", len(set(outliers[1])), " channels.")
+        occurences = [(Counter(outliers[1])[x], x) for x in list(Counter(outliers[1]))]
+        for occur, channel in occurences:
+            if occur > max_bad_episodes:
+                print("Found bad channel (more than", max_bad_episodes, " bad episodes): Channel no: ", channel )
+                bad_channels.append(channel)
+            else:
+                # only add bad episodes for non-bad channels
+                bad_episodes = bad_episodes|set(outliers[0][outliers[1] == channel])
+        
+        # Remove bad data:
+        signals = np.delete(signals, bad_channels, 1)
+        signals = np.delete(signals, list(bad_episodes), 0)
+        
+    else:
+        print("No outliers found with given threshold.")
+    
+    return signals, bad_channels, list(bad_episodes)
